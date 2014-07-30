8155d2f93a4571b4e6d73cc20960ba0c6dd45897
michael <michaeljearnshaw@gmail.com>
2014-07-30 18:11:23 +0100

    LAM-47 initial check in WCF Service - WS-HTTP Binding GetEmployee GetEmployees

---
+++ Hermes/HermesLib/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HermesLib
+{
+    public class Employee
+    {
+
+        private int _id;
+
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                if (_id == value)
+                {
+                    return;
+                }
+                _id = value;
+            }
+        }
+
+        
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set {
+                    if (_name == value)
+                    {
+                        return;
+                    }
+                  _name = value;
+                }
+        }
+
+        public Employee()
+        {
+            _id = 0;
+            _name = "name";
+
+        }
+
+        public Employee(int id)
+        {
+            ID = id;
+            Name = "name"+ID;
+
+        }
+
+    }
+}
---
+++ Hermes/HermesLib/IService1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HermesLib
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+        [OperationContract]
+        Employee GetEmployee();
+        [OperationContract]
+        ObservableCollection<Employee> GetEmployees();
+
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "HermesLib.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
